49b3fa5b5f2bf8bf9001311e129423e8683aa2d7
sahumayank <42769416+sahumayank@users.noreply.github.com>
2019-08-06 13:15:05 +0530

    Update DataTypes.ts

--- 01-Basics/DataTypes.ts
+++ 01-Basics/DataTypes.ts
@@ -12,7 +12,7 @@ let third:number=0o377 ;      // octal
 let fourth: number = 0b111001;// binary  
 
 
-console.log(first);  // 123 
-console.log(second); // 14287
-console.log(third);  // 255
-console.log(fourth); // 57 
+document.write(first);  // 123 
+document.write(second); // 14287
+document.write(third);  // 255
+document.write(fourth); // 57 
