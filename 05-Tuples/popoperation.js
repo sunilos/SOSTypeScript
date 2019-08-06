c6568a56e497e324d7d53e1ba56ef4f8f6e33fea
sahumayank <42769416+sahumayank@users.noreply.github.com>
2019-08-06 14:56:18 +0530

    Update popoperation.js

--- 05-Tuples/popoperation.js
+++ 05-Tuples/popoperation.js
@@ -5,18 +5,17 @@ This Example shows how to add element in existing list
 @Copyright (c) SunilOS
 @Url www.SunilOs.com
 */
-function GetElement(no) {
-    var empTuple = ["Ram Sharma", 25, "Typescript", "Python"];
-    console.log("Items: " + empTuple);
-    document.write("<h4>Element List Before Pop Operation -  " + empTuple + '</h4>');
+function GetElement(empTuple)
+{
+document.write("Items: "+empTuple);  
+document.write("Tuple pop Items  : "+ empTuple.pop()); // removed value to the tuple   
+document.write("Length of Tuple Items before pop: "+empTuple.length);   // returns the tuple size  
+document.write("Tuple pop Items  : "+ empTuple.pop()); // removed value to the tuple   
+document.write("Length of Tuple Items after pop: "+empTuple.length); 
+document.write("Items: "+empTuple);  
 
-    console.log("Length of Tuple Items before pop: " + empTuple.length); // returns the tuple size  
-     empTuple.pop(); // removed value to the tuple   
-    document.write("<h4>Show Empty List After Pop Operation - " + empTuple.length + '</h4>');
-    console.log(empTuple.length);
-    console.log("Items: " + empTuple);
-    //Clear  or empty  the field of tuples
+//Clear  or empty  the field of tuples
+
 }
-var empTuple1 = ["Shyam Sharma", 25, "Typscipt"];
-empTuple1 = [];
-console.log(empTuple1);
+var empTuple = ["Ram Sharma", 25, "Typescript", "Python"];  
+GetElement(empTuple)
