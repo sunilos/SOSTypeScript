f12d6339aa3d16b56745063113acb8dae905810a
sahumayank <42769416+sahumayank@users.noreply.github.com>
2019-08-06 15:03:18 +0530

    Update forloop.js

--- 03-ControlStmt/forloop.js
+++ 03-ControlStmt/forloop.js
@@ -1,6 +1,6 @@
 
 for (var i = 0; i < 5; i++) {
-    console.log("Hello");
+    document.write("Hello");
 }
 //Example of For of Loop using  Array to print  index values
 var arr = [10, 20, 30, 40];
@@ -8,6 +8,6 @@ document.write("For Loop using index values - " );
 for (var _i = 0, arr_1 = arr; _i < arr_1.length; _i++) {
     var val = arr_1[_i];
     document.write( val + '\n');
-    console.log(val + '\n'); // prints values: 10, 20, 30, 40
+    document.write(val + '\n'); // prints values: 10, 20, 30, 40
 }
 document.write('<br>')
