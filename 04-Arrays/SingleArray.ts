3cbe84fbc95349029fd364f7a91f37245bd358b0
sahumayank <42769416+sahumayank@users.noreply.github.com>
2019-08-06 15:07:49 +0530

    Update SingleArray.ts

--- 04-Arrays/SingleArray.ts
+++ 04-Arrays/SingleArray.ts
@@ -7,6 +7,7 @@
  */
 
 let arr:number[];   
-arr = [1, 2, 3, 4];   
+arr = [1, 2, 3, 4]; 
+document.write("<b>Single Array values - <br><b>");
 document.write("Array[0]: " +arr[0] + '<br>');   
 document.write("Array[1]: " +arr[1]);  
