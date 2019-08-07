7ff8f2cae2b2229e4680436d3ef477f2ab37202b
sahumayank <42769416+sahumayank@users.noreply.github.com>
2019-08-07 15:55:44 +0530

    Update DefaultParameter.ts

--- 07-Function/DefaultParameter.ts
+++ 07-Function/DefaultParameter.ts
@@ -10,9 +10,7 @@ function ShowName(name: string, greeting: string = "Hello") : string {
     return greeting + ' ' + name + '!';
 }
 
-ShowName('Ram'); //OK, returns "Hello Ram!"
-ShowName('Ram', 'Hi'); // OK, returns "Hi Ram!".
-ShowName('Shyam'); //OK, returns "Hello Shyam!"
+
 console.log(ShowName('Ram'));
 console.log(ShowName('Ram', 'Hi'));
 console.log(ShowName('Shyam'));
